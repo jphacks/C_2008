1b10cde975e7072d9a7310d31779cd294461b7e0
Saji Ayahito <ayahito_saji@outlook.jp>
2020-11-01 01:58:30 +0900

    facemesh.htmlとWebGazerの追加

---
+++ WebGazer/src/params.mjs
@@ -0,0 +1,24 @@
+const params = {
+  moveTickSize: 50,
+  videoElementId: 'webgazerVideoFeed',
+  videoElementCanvasId: 'webgazerVideoCanvas',
+  faceOverlayId: 'webgazerFaceOverlay',
+  faceFeedbackBoxId: 'webgazerFaceFeedbackBox',
+  gazeDotId: 'webgazerGazeDot',
+  videoViewerWidth: 320,
+  videoViewerHeight: 240,
+  faceFeedbackBoxRatio: 0.66,
+  // View options
+  showVideo: true,
+  mirrorVideo: true,
+  showFaceOverlay: true,
+  showFaceFeedbackBox: true,
+  showGazeDot: true,
+  camConstraints: { video: { width: { min: 320, ideal: 640, max: 1920 }, height: { min: 240, ideal: 480, max: 1080 }, facingMode: "user" } },
+  dataTimestep: 50,
+  showVideoPreview: false,
+  // Whether or not to store accuracy eigenValues, used by the calibration example file
+  storingPoints: false,
+};
+
+export default params;
