cdeec5b159c1f1a8fb6a9e31c2545c85fbc4fbaa
sakata yuma <skt97yuma@outlook.jp>
2020-11-01 23:07:04 +0900

    mがなかったので追加したよ！ちゃんとして！

--- comicse/src/libraries/regression.js
+++ comicse/src/libraries/regression.js
@@ -41,6 +41,7 @@ export default {
   }),
   predict: (async (input) => { // 予測関数（inputは10*6*2次元のベクトル）
     // input[10]...入力の10番目の数値
+    let m = input.length;
     const result = model.predict(tf.tensor2d(input, [1,m])).arraySync();
     result[0] = Math.max(0, Math.min(result[0], 100));
     result[1] = Math.max(0, Math.min(result[1], 100));
