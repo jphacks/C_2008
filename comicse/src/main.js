1d3751e1362d695bdd8f799c9e3015e7d3c2362d
Saji Ayahito <ayahito_saji@outlook.jp>
2020-11-01 17:09:52 +0900

    tf-layersとelementJSを導入

--- comicse/src/main.js
+++ comicse/src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import locale from 'element-ui/lib/locale/lang/ja'
+
+Vue.use(ElementUI, { locale })
+
 Vue.config.productionTip = false
 
 new Vue({
