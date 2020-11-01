2d883386edb9270803a2858736e6c6b7924409ba
sakata yuma <skt97yuma@outlook.jp>
2020-11-01 23:14:13 +0900

    predictを関数化してデバッグしやすく？

--- comicse/src/libraries/regression.js
+++ comicse/src/libraries/regression.js
@@ -35,16 +35,20 @@ export default {
     const history = await model.fit(X, Y, { epochs: 100 });
 
     console.log(history.history.loss);
-    const result = model.predict(tf.tensor2d(inputs[0], [1,m])).arraySync();
-    console.log([result[0][0], result[0][1]]);
+    const result = predict(inputs[0]);
+    console.log(result);
 
   }),
   predict: (async (input) => { // 予測関数（inputは10*6*2次元のベクトル）
     // input[10]...入力の10番目の数値
-    let m = input.length;
-    const result = model.predict(tf.tensor2d(input, [1,m])).arraySync();
-    result[0] = Math.max(0, Math.min(result[0], 100));
-    result[1] = Math.max(0, Math.min(result[1], 100));
-    return [result[0], result[1]];
+    return predict(input);
   })
+}
+
+function predict(input){
+  let m = input.length;
+  const result = model.predict(tf.tensor2d(input, [1,m])).arraySync();
+  result[0] = Math.max(0, Math.min(result[0], 100));
+  result[1] = Math.max(0, Math.min(result[1], 100));
+  return [result[0], result[1]];
 }
