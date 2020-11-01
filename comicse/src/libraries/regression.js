2557c1ed44345b8f2314f74f07160497d77d9bca
sakata yuma <skt97yuma@outlook.jp>
2020-11-01 23:01:54 +0900

    モデルはregression.jsで保存するようにしたよ！めんどいね！

--- comicse/src/libraries/regression.js
+++ comicse/src/libraries/regression.js
@@ -38,12 +38,10 @@ export default {
     const result = model.predict(tf.tensor2d(inputs[0], [1,m])).arraySync();
     console.log([result[0][0], result[0][1]]);
 
-    return model;
-
   }),
-  predict: (async (input, learnModel) => { // 予測関数（inputは10*6*2次元のベクトル）
+  predict: (async (input) => { // 予測関数（inputは10*6*2次元のベクトル）
     // input[10]...入力の10番目の数値
-    const result = learnModel.predict(tf.tensor2d(input, [1,m])).arraySync();
+    const result = model.predict(tf.tensor2d(input, [1,m])).arraySync();
     result[0] = Math.max(0, Math.min(result[0], 100));
     result[1] = Math.max(0, Math.min(result[1], 100));
     return [result[0], result[1]];
