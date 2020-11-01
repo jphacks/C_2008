4add8adab7d26e55b33051d02ff772a18aea7ac8
Saji Ayahito <ayahito_saji@outlook.jp>
2020-11-01 16:34:57 +0900

    ヒストグラムフィルターの実装

--- comicse/src/components/FaceMesh.vue
+++ comicse/src/components/FaceMesh.vue
@@ -33,9 +33,11 @@
         if (this.ready !== true) return
 
         if (this.video === null) return
+        // FaceMeshを利用して顔の位置を推定
         const faces = await this.model.estimateFaces(this.video)
         if (faces.length === 0) return
 
+        // 0番目の顔のポジションから左目の位置と右目の位置を算出
         const positions = faces[0].scaledMesh
         const leftX  = Math.round(Math.min(positions[247][0], positions[130][0], positions[25][0]))
         const leftY  = Math.round(Math.min(positions[247][1], positions[27][1], positions[190][1]))
@@ -49,22 +51,51 @@
         if (leftW === 0 || rightW === 0 || leftH === 0 || rightH === 0) return
 
         if (this.eyeL === null || this.eyeR === null) return
+
+        // this.eyeWidth*this.eyeHeightにリサイズしてカンバスに描写
         this.eyeL.getContext('2d').drawImage(this.video, leftX, leftY, leftW, leftH, 0, 0, this.eyeWidth, this.eyeHeight)
         this.eyeR.getContext('2d').drawImage(this.video, rightX, rightY, rightW, rightH, 0, 0, this.eyeWidth, this.eyeHeight)
 
+        // カンバスをデータ化して特徴量ベクトルeyeに変換する
         const eyes = this.imageData2array(this.eyeL.getContext('2d').getImageData(0, 0, this.eyeWidth, this.eyeHeight)).concat(
           this.imageData2array(this.eyeR.getContext('2d').getImageData(0, 0, this.eyeWidth, this.eyeHeight))
         )
 
+        //  eyesを渡す
         this.$emit("getEyes", eyes)
         return eyes
       },
+      // 片目の画像データから特徴量ベクトルに変換する
       imageData2array: function (imageData) {
-        let retval = new Array(this.eyeWidth * this.eyeHeight)
-        for (let i = 0;i < this.eyeWidth * this.eyeHeight;i ++) {
-          retval[i] = (imageData.data[i * 4] + imageData.data[i * 4 + 1] + imageData.data[i*4 + 2]) / 3 / 256
+        const IMAGE_DATA_LENGTH = this.eyeWidth * this.eyeHeight
+        const BRIGHTNESS = 256
+        const STEP = 5
+
+        let gray = new Array(IMAGE_DATA_LENGTH)
+        let histogram = new Array(BRIGHTNESS).fill(0)
+
+        // グレースケール化（明度データ）
+        for (let i = 0;i < IMAGE_DATA_LENGTH;i ++) {
+          gray[i] = Math.floor((imageData.data[i * 4] * 0.299 + imageData.data[i * 4 + 1] * 0.587 + imageData.data[i * 4 + 2] * 0.114))
         }
-        return retval
+
+        // ヒストグラムを作成（ただし全ての点を使わず，5つに1つの割合で使う）
+        for (let i = 0;i < IMAGE_DATA_LENGTH;i += STEP) {
+          histogram[gray[i]] ++;
+        }
+        // ヒストグラムを積み上げてフィルターを作成
+        let sum = 0
+        for (let j = 0;j < BRIGHTNESS;j++) {
+          sum += histogram[j]
+          histogram[j] = Math.floor(sum / (IMAGE_DATA_LENGTH / STEP) * BRIGHTNESS)
+        }
+        // フィルターに明度データを通す
+        let filtered_gray = new Array(IMAGE_DATA_LENGTH)
+        for (let i = 0;i < IMAGE_DATA_LENGTH;i ++) {
+          filtered_gray[i] = histogram[gray[i]]
+        }
+
+        return filtered_gray
       }
     },
     mounted: async function () {
