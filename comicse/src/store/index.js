674dc448598c2374688702a7728d4816113badd3
Saji Ayahito <ayahito_saji@outlook.jp>
2020-11-01 15:25:21 +0900

    キャリブレーションデータをブラウザに保存する機能

--- comicse/src/store/index.js
+++ comicse/src/store/index.js
@@ -1,12 +1,30 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from "vuex-persistedstate";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  plugins: [createPersistedState()],
   state: {
+    calibrateInputs: [],
+    calibrateOutputs: [],
+    weight: null,
   },
   mutations: {
+    resetCalibrateData: function ( state ) {
+      state.calibrateInputs = []
+      state.calibrateOutputs = []
+    },
+    pushCalibrateData: function ( state, payload ) {
+      if (payload && payload.input && payload.output) {
+        state.calibrateInputs.push(payload.input)
+        state.calibrateOutputs.push(payload.output)
+      }
+    },
+    setWeight: function ( state, payload ) {
+      state.weight = payload.weight
+    }
   },
   actions: {
   },
--- comicse/src/views/Calibrate.vue
+++ comicse/src/views/Calibrate.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="calibrate">
-    <FaceMesh ref="facemesh" @onready="ready"/>
+    <FaceMesh ref="facemesh" @onready="onReadyCalibrate"/>
     <canvas class="calibrate-screen" ref="calibrate_screen" @click="calibrate"></canvas>
+    <div class="description">
+      ■キャリブレーション<br/>
+      データ数: {{ calibrateInputs.length }}<br/>
+      キャリブレーションしたデータは<br/>
+      自動で保存されます．<br/>
+      <button @click="resetCalibrateData">data reset</button>
+    </div>
   </div>
 </template>
 
@@ -12,15 +19,26 @@
     components: {
       FaceMesh
     },
+    data () {
+      return {
+        ready: false
+      }
+    },
+    computed: {
+      calibrateInputs: function () { return this.$store.state.calibrateInputs },
+      calibrateOutputs: function () { return this.$store.state.calibrateOutputs }
+    },
     mounted () {
       this.$refs.calibrate_screen.width = window.innerWidth
       this.$refs.calibrate_screen.height = window.innerHeight
     },
     methods: {
-      ready: function () {
+      onReadyCalibrate: function () {
+        this.ready = true
         console.log("準備ができました．クリックして目のキャリブレーションを行ってください")
       },
       calibrate: async function (e) {
+        if (this.ready === false) return
         console.log("キャリブレーション")
         const input = await this.$refs.facemesh.getEyes()
         const output = [e.pageX / window.innerWidth * 100, e.pageY / window.innerHeight * 100]
@@ -28,6 +46,13 @@
         console.log("入力: ", input)
         console.log("出力: ", output)
         console.log("重み: ", weight)
+
+        this.$store.commit("pushCalibrateData", { input: input, output: output })
+        this.$store.commit("setWeight", { weight: weight })
+
+      },
+      resetCalibrateData: function () {
+        this.$store.commit("resetCalibrateData")
       }
     }
   }
@@ -45,5 +70,11 @@
     position: absolute;
     top: 0;
     left: 0;
+  }
+  .description {
+    position: absolute;
+    top: 0;
+    left: 0;
+    text-align: left;
   }
 </style>
